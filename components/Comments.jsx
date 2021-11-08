042ff3ad2a68ba15131ab457bc652ac0241559f2
coffmanjrp <coffmanjrp@gmail.com>
2021-11-07 17:49:54 -0800

    Fix Comments component

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,5 +1,40 @@
-const Comments = () => {
-  return <div>Comments</div>;
+import { useEffect, useState } from 'react';
+import moment from 'moment';
+import parse from 'html-react-parser';
+import { getComments } from '../services';
+
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result));
+  }, []);
+
+  return (
+    <>
+      {comments.length > 0 && (
+        <div className="bg-white shadow-lg p-8 pb-12 mb-8 rounded-lg">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+            {comments.length} Comments
+          </h3>
+          {comments.map((comment) => (
+            <div
+              className="mb-4 border-b border-gray-100 pb-4"
+              key={comment.createdAt}
+            >
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span> on{' '}
+                {moment(comment.createdAt).format('MMM DD, YYYY')}
+              </p>
+              <p className="whitespace-pre-line text-gray-600 w-full">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Comments;
